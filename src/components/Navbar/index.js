c3ac3fc2f1fb893a23e9d553ac6af79220e28542
glauber2k2 <devglaubermonteiro@gmail.com>
2023-09-23 13:44:50 -0300

    refatoring presentation for tailwind

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -131,7 +131,7 @@ export default function Navbar() {
         >
           <button
             onClick={abrirWpp}
-            className='flex justify-center items-center w-52 h-10 sm:h-12 sm:w-72 gap-4 rounded-lg bg-white text-black text-lg font-medium hover:text-purple-800 sm:mt-0 mt-8'
+            className='flex justify-center items-center w-52 h-10 sm:h-12 sm:w-72 gap-4 rounded-lg bg-white hover:bg-purple-500 text-black text-lg font-medium sm:mt-0 mt-8'
           >
             <WhatsappLogo weight='fill' className='w-6 h-6' />
             Fale comigo!
@@ -140,7 +140,7 @@ export default function Navbar() {
           <div className='flex gap-8 items-center justify-center sm:gap-4 mt-4 sm:mt-0'>
             <button
               onClick={abrirInsta}
-              className='bg-white rounded-full text-black w-10 h-10 sm:h-12 sm:w-12 flex justify-center items-center hover:text-purple-800'
+              className='bg-white hover:bg-purple-500 rounded-full text-black w-10 h-10 sm:h-12 sm:w-12 flex justify-center items-center'
             >
               <InstagramLogo
                 weight='fill'
@@ -149,22 +149,22 @@ export default function Navbar() {
             </button>
             <button
               onClick={abrirGit}
-              className='bg-white rounded-full text-black w-10 h-10 sm:h-12 sm:w-12 flex justify-center items-center hover:text-purple-800'
+              className='bg-white hover:bg-purple-500 rounded-full text-black w-10 h-10 sm:h-12 sm:w-12 flex justify-center items-center'
             >
               <svg
-                xmlns='http://www.w3.org/2000/svg'
                 x='0px'
                 y='0px'
                 width='100'
                 height='100'
                 viewBox='0 0 72 72'
+                className='w-10 h-10'
               >
                 <path d='M36,12c13.255,0,24,10.745,24,24c0,10.656-6.948,19.685-16.559,22.818c0.003-0.009,0.007-0.022,0.007-0.022	s-1.62-0.759-1.586-2.114c0.038-1.491,0-4.971,0-6.248c0-2.193-1.388-3.747-1.388-3.747s10.884,0.122,10.884-11.491	c0-4.481-2.342-6.812-2.342-6.812s1.23-4.784-0.426-6.812c-1.856-0.2-5.18,1.774-6.6,2.697c0,0-2.25-0.922-5.991-0.922	c-3.742,0-5.991,0.922-5.991,0.922c-1.419-0.922-4.744-2.897-6.6-2.697c-1.656,2.029-0.426,6.812-0.426,6.812	s-2.342,2.332-2.342,6.812c0,11.613,10.884,11.491,10.884,11.491s-1.097,1.239-1.336,3.061c-0.76,0.258-1.877,0.576-2.78,0.576	c-2.362,0-4.159-2.296-4.817-3.358c-0.649-1.048-1.98-1.927-3.221-1.927c-0.817,0-1.216,0.409-1.216,0.876s1.146,0.793,1.902,1.659	c1.594,1.826,1.565,5.933,7.245,5.933c0.617,0,1.876-0.152,2.823-0.279c-0.006,1.293-0.007,2.657,0.013,3.454	c0.034,1.355-1.586,2.114-1.586,2.114s0.004,0.013,0.007,0.022C18.948,55.685,12,46.656,12,36C12,22.745,22.745,12,36,12z'></path>
               </svg>
             </button>
             <button
               onClick={abrirLinkedin}
-              className='bg-white rounded-full text-black w-10 h-10 sm:h-12 sm:w-12 flex justify-center items-center hover:text-purple-800'
+              className='bg-white hover:bg-purple-500 rounded-full text-black w-10 h-10 sm:h-12 sm:w-12 flex justify-center items-center'
             >
               <LinkedinLogo weight='fill' className=' w-4 h-4 sm:w-6 sm:h-6 ' />
             </button>
@@ -176,7 +176,7 @@ export default function Navbar() {
         weight='bold'
         data-aos='fade-up'
         data-aos-delay='2000'
-        className='absolute left-[50%] animate-bounce w-8 h-8 sm:w-16 sm:h-16 cursor-pointer'
+        className='absolute left-[50%] sm:left-[49%] animate-bounce  sm:w-8 sm:h-8 2xl:w-10 2xl:h-10 cursor-pointer'
         onClick={() => {
           const targetElement = document.getElementById('skills')
           if (targetElement) {
--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,90 +1,103 @@
 import Image from 'next/image'
 
-import styles from './Presentation.module.css'
-
 import { FileJs, FileTs, AngularLogo, Atom, FigmaLogo } from 'phosphor-react'
 
 export default function Presentation() {
   return (
-    <main className={styles.container} id='skills'>
-      <div className={styles.skills} data-aos='fade-up' data-aos-delay='100'>
+    <main
+      className='flex flex-col w-full h-full py-12 px-8 gap-[3vh]'
+      id='skills'
+    >
+      <div
+        className='relative bg-black p-2 text-black flex justify-center gap-4 before:content-["Habilidades"] before:text-white before:text-xl before:absolute before:top-[-32px] before:w-full before:text-center  before:bg-black before:px-1 flex-wrap'
+        data-aos='fade-up'
+        data-aos-delay='100'
+      >
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
             <svg
               x='0px'
               y='0px'
               width='100'
               height='100'
               viewBox='0 0 48 48'
-              className='w-12 h-12'
+              className='sm:w-12 sm:h-12 w-10 h-10'
             >
               <path
                 fill='#000000'
                 d='M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z'
               ></path>
             </svg>
-            <p>Nextjs</p>
+            <p className='sm:flex hidden'>Nextjs</p>
           </span>
         </div>
 
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
-            <Atom className='w-12 h-12' weight='fill' />
-            <p>React</p>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
+            <Atom className='sm:w-12 sm:h-12 w-10 h-10' weight='fill' />
+            <p className='sm:flex hidden'>React</p>
           </span>
         </div>
 
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
-            <FileJs className='w-12 h-12' weight='fill' />
-            <p>Javascript</p>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
+            <FileJs className='sm:w-12 sm:h-12 w-10 h-10' weight='fill' />
+            <p className='sm:flex hidden'>Javascript</p>
           </span>
         </div>
 
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
-            <FileTs className='w-12 h-12' weight='fill' />
-            <p>Typescript</p>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
+            <FileTs className='sm:w-12 sm:h-12 w-10 h-10' weight='fill' />
+            <p className='sm:flex hidden'>Typescript</p>
           </span>
         </div>
 
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
             <svg
               x='0px'
               y='0px'
               width='100'
               height='100'
               viewBox='0 0 24 24'
-              className='w-12 h-12'
+              className='sm:w-12 sm:h-12 w-10 h-10'
             >
               <path d='M23.395 7.081c-.488-.207-1.053-.002-1.29.472-.224.448-.523.723-.914.838-.612.183-1.343-.052-1.685-.253-.451-.265-.974-.667-1.527-1.092C16.214 5.688 14.018 4 11 4 8.586 4 7.346 5.239 5.293 7.293 4.902 7.684 4.899 8.32 5.29 8.71 5.67 9.092 6.28 9.104 6.672 8.74c.01-.009.02-.019.03-.028.552-.426 4.03-.012 5.55 1.196C14.511 11.703 16.142 13 18 13c2.659 0 4.879-1.741 5.94-4.658C24.121 7.844 23.882 7.291 23.395 7.081zM18.395 14.081c-.488-.207-1.053-.002-1.29.472-.224.448-.523.723-.914.838-.612.18-1.343-.052-1.685-.253-.451-.265-.974-.667-1.527-1.092C11.214 12.688 9.018 11 6 11c-2.414 0-3.654 1.239-5.707 3.293-.391.391-.394 1.027-.003 1.417.38.382.991.395 1.383.03.01-.009.02-.019.03-.028.551-.426 4.031-.012 5.55 1.196C9.511 18.703 11.142 20 13 20c2.659 0 4.879-1.741 5.94-4.658C19.121 14.844 18.882 14.291 18.395 14.081z'></path>
             </svg>
-            <p>TailwindCSS</p>
+            <p className='sm:flex hidden'>TailwindCSS</p>
           </span>
         </div>
 
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
-            <AngularLogo className='w-12 h-12' weight='fill' />
-            <p>Angular</p>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
+            <AngularLogo className='sm:w-12 sm:h-12 w-10 h-10' weight='fill' />
+            <p className='sm:flex hidden'>Angular</p>
           </span>
         </div>
 
         <div data-aos='fade-up' data-aos-delay='100'>
-          <span>
-            <FigmaLogo className='w-12 h-12' weight='fill' />
-            <p>Figma</p>
+          <span className='flex w-14 sm:w-[10vw] h-32 sm:h-[16vw] flex-col justify-center items-center gap-2 ease-out duration-300 bg-purple-500 select-none hover:scale-110'>
+            <FigmaLogo className='sm:w-12 sm:h-12 w-10 h-10' weight='fill' />
+            <p className='sm:flex hidden'>Figma</p>
           </span>
         </div>
       </div>
 
-      <div className={styles.diferentials}>
-        <span data-aos='fade-up' data-aos-delay='100'>
-          <h1 data-aos='fade-up' data-aos-delay='100'>
+      <div className='flex items-center  justify-center sm:justify-evenly w-full h-full sm:gap-12 gap-8 flex-wrap'>
+        <span
+          data-aos='fade-up'
+          data-aos-delay='100'
+          className='relative flex flex-col items-center border border-purple-600 w-80 sm:w-[30%] h-[90%]'
+        >
+          <h1
+            data-aos='fade-up'
+            data-aos-delay='100'
+            className='sm:text-[4vw] text-purple-500 p-4 sm:p-8 text-center sm:text-start text-[46px]'
+          >
             Imagens com IA
           </h1>
-          <p>
+          <p className='sm:text-[1.2vw] text-purple-500 sm:p-8 text-justify text-[16px] p-4 sm:text-start'>
             Transformo seu site em uma obra de arte visual, criando imagens
             únicas e impactantes utlizando inteligência artificial.
           </p>
@@ -93,12 +106,18 @@ export default function Presentation() {
             width={1280}
             height={1280}
             alt='ia'
-            className={styles.ia}
+            className='flex absolute object-scale-down w-full sm:h-full h-48 top-[-15%] sm:top-[-25%]'
           />
         </span>
-        <span data-aos='fade-up' data-aos-delay='100'>
-          <h1>Percepção de Design</h1>
-          <p>
+        <span
+          data-aos='fade-up'
+          data-aos-delay='100'
+          className='relative flex flex-col items-center border border-purple-600 w-80 sm:w-[30%] h-[90%]'
+        >
+          <h1 className='sm:text-[4vw] text-purple-500 p-4 sm:p-8 text-center sm:text-start text-[46px]'>
+            Percepção de Design
+          </h1>
+          <p className='sm:text-[1.2vw] text-purple-500 sm:p-8 text-justify text-[16px] p-4 sm:text-start'>
             Transformo seu site em uma obra de arte visual, criando imagens
             únicas e impactantes utlizando inteligência artificial.
           </p>
@@ -108,20 +127,30 @@ export default function Presentation() {
             width={1280}
             height={1280}
             alt='design'
-            className={styles.design}
+            className='flex absolute object-scale-down w-1/2 sm:h-1/2 h-2/5 bottom-[-5%] left-[-20%]'
           />
         </span>
 
         <div
           data-aos='fade-up'
           data-aos-delay='100'
-          className={styles.detailsH1}
+          className='md:flex hidden flex-col text-[1.9vw] gap-[1vw]'
         >
-          <h1 className={styles.fillh1}>DIFERENCIAIS</h1>
-          <h1 className={styles.fillh1}>DIFERENCIAIS</h1>
-          <h1 className={styles.fillh1}>DIFERENCIAIS</h1>
-          <h1 className={styles.fillh1}>DIFERENCIAIS</h1>
-          <h1 className={styles.fillh1}>DIFERENCIAIS</h1>
+          <h1 className='font-semibold text-purple-200 opacity-30'>
+            DIFERENCIAIS
+          </h1>
+          <h1 className='font-semibold text-purple-500 opacity-30'>
+            DIFERENCIAIS
+          </h1>
+          <h1 className='font-semibold text-purple-200 opacity-30'>
+            DIFERENCIAIS
+          </h1>
+          <h1 className='font-semibold text-purple-500 opacity-30'>
+            DIFERENCIAIS
+          </h1>
+          <h1 className='font-semibold text-purple-200 opacity-30'>
+            DIFERENCIAIS
+          </h1>
         </div>
       </div>
     </main>

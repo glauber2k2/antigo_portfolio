527890ddf3b67422f9128ff3e48a96ab67505cbc
glauber2k2 <devglaubermonteiro@gmail.com>
2023-09-24 14:34:16 -0300

    active and adjust AOS

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,19 +9,27 @@ export default function Projects() {
     >
       {/* <Image src={} width={} height={} alt /> */}
 
-      <div
-        className='flex flex-col sm:w-1/2 w-full h-1/2 sm:h-full justify-center items-center p-4 gap-y-2'
-        // data-aos='fade-up'
-        // data-aos-delay='100'
-      >
-        <h1 className='sm:text-[4vw] text-3xl font-medium xl:h-20'>
+      <div className='flex flex-col sm:w-1/2 w-full h-1/2 sm:h-full justify-center items-center p-4 gap-y-2'>
+        <h1
+          className='sm:text-[4vw] text-3xl font-medium xl:h-20'
+          data-aos='fade-up'
+          data-aos-delay='200'
+        >
           MEUS <span className='bg-purple-500 text-black'>PROJETOS</span>
         </h1>
-        <h2 className='text-base sm:text-[1.5vw] font-medium xl:mb-4'>
+        <h2
+          className='text-base sm:text-[1.5vw] font-medium xl:mb-4'
+          data-aos='fade-up'
+          data-aos-delay='250'
+        >
           use o notebook abaixo!
         </h2>
 
-        <h3 className='w-4/5 text-xs sm:text-[1vw] text-center'>
+        <h3
+          className='w-4/5 text-xs sm:text-[1vw] text-center'
+          data-aos='fade-up'
+          data-aos-delay='300'
+        >
           Para visualziar meus projetos, role a tela do notebook. e veja como um
           site bem construido pode passar confiança a quem está acessando.
         </h3>
@@ -30,9 +38,9 @@ export default function Projects() {
       <div className='sm:w-1/2 w-full flex justify-center items-center h-1/2 sm:h-full'>
         <div
           className='w-[80%]  flex'
-          // data-aos='zoom-in-up'
-          // data-aos-delay='100'
-          // data-aos-duration='1000'
+          data-aos='zoom-in-up'
+          data-aos-delay='500'
+          data-aos-duration='1000'
         >
           <ProjectWindow />
           {/* <CaretDoubleDown className='w-12 h-12' weight='fill' /> */}

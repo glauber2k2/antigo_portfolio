bdda73449b3e62a71a9131ca2ce9a09027bd0dde
Glauber Monteiro <103267667+glauber2k2@users.noreply.github.com>
2023-09-27 10:53:10 -0300

    Update notebook screens

--- src/components/ProjectWindow/index.jsx
+++ src/components/ProjectWindow/index.jsx
@@ -1,78 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import styles from './ProjectWindow.module.css';
-import Link from 'next/link';
+import Image from 'next/image'
+import Link from 'next/link'
+import { Lock } from 'phosphor-react'
+import { useEffect, useState } from 'react'
+import styles from './ProjectWindow.module.css'
 
 export default function ProjectWindow() {
-  const [hoveredItem, setHoveredItem] = useState(null);
-  const [detalhesRepositorios, setDetalhesRepositorios] = useState([]);
+  const [hoveredItem, setHoveredItem] = useState(null)
+  const [repositories, setRepositories] = useState([])
 
   useEffect(() => {
-    // Função para fazer uma requisição e lidar com erros
-    const fetchWithAuth = async (url) => {
-      try {
-        const response = await fetch(url);
+    // Fazer uma solicitação usando fetch
+    fetch('https://api.github.com/users/glauber2k2/repos')
+      .then((response) => {
         if (!response.ok) {
-          throw new Error(`Erro na requisição para ${url}`);
+          throw new Error('Não foi possível obter os repositórios.')
         }
-        return await response.json();
-      } catch (error) {
-        console.error('Erro:', error);
-        return null;
-      }
-    };
-
-    // Buscar lista de repositórios
-    fetchWithAuth('https://api.github.com/users/glauber2k2/repos')
-      .then((data) => {
-        if (data) {
-          // Filtrar repositórios que começam com "_"
-          const filteredRepositories = data.filter((repo) => repo.description.startsWith('*'));
-          const repositoryNames = filteredRepositories.map((repo) => repo.name);
-          return Promise.all(
-            repositoryNames.map((name) =>
-              fetchWithAuth(`https://api.github.com/repos/glauber2k2/${name}`)
-            )
-          );
-        }
-        return [];
+        return response.json()
       })
-      .then((responses) => {
-        const validResponses = responses.filter((response) => response !== null);
-        setDetalhesRepositorios(validResponses);
-      });
-  }, []);
+      .then((data) => {
+        // Filtrar os repositórios com descrição começada por "*"
+        const filteredRepositories = data.filter(
+          (repo) => repo.description && repo.description.startsWith('*')
+        )
+        setRepositories(filteredRepositories)
+        // Armazenar os repositórios filtrados no estado "repositories"
+      })
+      .catch((error) => {
+        console.error(error)
+        setRepositories(null)
+      })
+  }, [])
 
   return (
     <div className={styles.container}>
-      <Image src="/images/laptop.png" width={1080} height={1080} alt="" />
+      <Image src='/images/laptop.png' width={1080} height={1080} alt='' />
       <div className={styles.window}>
-        <div className={styles.content}>
-          <Image src="/images/mylogo.png" width={1080} height={1080} alt="" />
-          <h1>ROLE P/ BAIXO</h1>
-        </div>
+        {repositories !== null && repositories.length !== 0 && (
+          <div className={styles.content}>
+            <Image src='/images/mylogo.png' width={1080} height={1080} alt='' />
+            <h1>ROLE P/ BAIXO</h1>
+          </div>
+        )}
 
-        {detalhesRepositorios.map((repo) => (
-          <Link key={repo.id} href={repo.homepage} legacyBehavior>
-            <a target="_blank">
-              <div
-                className={`${styles.content} ${hoveredItem === repo.name ? styles.hovered : ''}`}
-                onMouseEnter={() => setHoveredItem(repo.name)}
-                onMouseLeave={() => setHoveredItem(null)}
-              >
-                <Image
-                  src={`https://raw.githubusercontent.com/glauber2k2/${repo.name}/main/public/thumb.png`}
-                  width={1080}
-                  height={1080}
-                  alt=""
-                />
-                <h1>{repo.name.split('_')}</h1>
-                {hoveredItem === repo.name && <p>{repo.topics.join(' - ')}</p>}
-              </div>
-            </a>
-          </Link>
-        ))}
+        {repositories !== null && repositories.length === 0 && (
+          <div className='h-full flex justify-center items-center gap-4'>
+            <div className='loader border-t-2 rounded-full border-purple-500  animate-spin aspect-square w-8 flex justify-center items-center text-yellow-700'></div>
+            <h1 className='text-sm animate-pulse'>Loading</h1>
+          </div>
+        )}
+
+        {repositories === null && (
+          <div className='h-full flex justify-center items-center gap-1 flex-col'>
+            <Lock className='w-10 h-10' weight='duotone' />
+            <h1 className='text-sm animate-pulse text-center'>
+              Falha ao conectar com o GitHub.
+            </h1>
+          </div>
+        )}
+
+        {repositories !== null
+          ? repositories.map((repo) => (
+              <Link key={repo.id} href={repo.homepage} legacyBehavior>
+                <a target='_blank'>
+                  <div
+                    className={`${styles.content} ${
+                      hoveredItem === repo.name ? styles.hovered : ''
+                    }`}
+                    onMouseEnter={() => setHoveredItem(repo.name)}
+                    onMouseLeave={() => setHoveredItem(null)}
+                  >
+                    <Image
+                      src={`https://raw.githubusercontent.com/glauber2k2/${repo.name}/main/public/thumb.png`}
+                      width={1080}
+                      height={1080}
+                      alt=''
+                    />
+                    <h1 className='capitalize'>{repo.name}</h1>
+                    {hoveredItem === repo.name && (
+                      <p className='capitalize'>
+                        {repo.topics.sort().join(' - ')}
+                      </p>
+                    )}
+                  </div>
+                </a>
+              </Link>
+            ))
+          : ''}
       </div>
     </div>
-  );
+  )
 }
